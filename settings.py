dee15411117b0be698b541e87edd4aaec8682b5c
snaiper04ek <snaiper04ek353@gmail.com>
2021-08-25 17:22:37 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 file_name = '\\\server\\Server\\_Василий IT\\От Чижова А\\Алютехник 02.08.2021(Наш вариант).xlsx'
 new_file = '\\\server\\Server\\_Василий IT\\От Чижова А\\Прайслист обработанный.xlsx'
 sheet_name = 'Sheet 1'
+value_to_roundnum = 2
